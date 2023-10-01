d19a67d20e1cc67e302907d6d7180aef66e3e606
Giulia Carlini <gcarlini>
2023-10-01 18:22:09 -0300

    Implementação do unitOfWork

--- Controllers/ImportarController.cs
+++ Controllers/ImportarController.cs
@@ -1,3 +1,4 @@
+using ControleFinanceiro.Domain.Data;
 using ControleFinanceiro.Domain.Interface;
 using ControleFinanceiro.Domain.Utils;
 using Microsoft.AspNetCore.Mvc;
@@ -9,28 +10,51 @@ namespace ControleFinanceiroAPI.Controllers
     public class ImportarController : ControllerBase
     {
         private readonly ILogger<ImportarController> _logger;
-        private readonly IImportar importarService;
+        private readonly IImportar _importarService;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public ImportarController(ILogger<ImportarController> logger, IImportar importarService)
+        public ImportarController(ILogger<ImportarController> logger, IImportar importarService, IUnitOfWork unitOfWork)
         {
             _logger = logger;
-            this.importarService = importarService;
+            _importarService = importarService;
+            _unitOfWork = unitOfWork;
         }
 
         [HttpPost("ImportarCSVNubank")]
         public ActionResult ImportarCSVNubank(string CaminhoArquivo, DateTime Vencimento)
         {
-            importarService.ImportarArquivo(CaminhoArquivo, Vencimento, TipoImportacao.Nubank);
+            _unitOfWork.BeginTransaction();
+            try
+            {
+                var idFatura = _importarService.ImportarArquivo(CaminhoArquivo, Vencimento, TipoImportacao.Nubank);
+                
+                _unitOfWork.Commit();
 
-            return Ok(); 
+                return idFatura > 0 ? Ok(idFatura) : NotFound("Fatura não importada.");
+            }
+            catch (Exception ex)
+            {
+                _unitOfWork.Rollback();
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost("ImportarCSVC6Bank")]
         public ActionResult ImportarCSVC6Bank(string CaminhoArquivo, DateTime Vencimento)
         {
-            importarService.ImportarArquivo(CaminhoArquivo, Vencimento, TipoImportacao.C6Bank);
+            _unitOfWork.BeginTransaction();
+            try
+            {
+                _importarService.ImportarArquivo(CaminhoArquivo, Vencimento, TipoImportacao.C6Bank);
+                _unitOfWork.Commit();
 
-            return Ok();
+                return Ok();
+            }
+            catch (Exception ex) 
+            {
+                _unitOfWork.Rollback();
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
